b6ad3dd6965dca4abcdf5fdb750ca1c75cde266b
StR0vErBG <xcreaw1@abv.bg>
2023-04-16 06:43:01 -0700

    adding loading state

---
+++ app/account/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='flex-center font-semibold text-xl'>
+            Зарежда се...
+        </div>
+    );
+}
+
+export default Loading;
