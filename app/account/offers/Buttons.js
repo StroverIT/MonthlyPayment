c7aea61d8285a68b568fff9c5277e4d2b4e8d4a6
StR0vErBG <xcreaw1@abv.bg>
2023-04-16 06:43:16 -0700

    adding buttons and links to them

--- app/account/offers/Buttons.js
+++ app/account/offers/Buttons.js
@@ -2,11 +2,14 @@
 
 import React from "react";
 import Button from "../../../components/Forms/Buttons/Default";
+import Link from "next/link";
 
 const Buttons = () => {
   return (
-    <div>
-      <Button text="test" />
+    <div className="flex gap-x-5 mb-5">
+      <Link href="/account/offers"><Button className="p-4 text-sm" text="Офертите ми" /></Link>
+      <Link href="/account/offers/create"><Button className="p-4 text-sm" text="Създай оферта" /></Link>
+
     </div>
   );
 };
