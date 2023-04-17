7f91e903229b97b0b22824bef28c58fcddedd3ba
StR0vErBG <xcreaw1@abv.bg>
2023-04-17 11:56:13 +0200

    fixing spacing and note information

--- app/account/offers/show/[id]/page.js
+++ app/account/offers/show/[id]/page.js
@@ -1,35 +1,47 @@
 import React from "react";
+import Button from "../../../../../components/Forms/Buttons/Default";
 
 const Page = () => {
   return (
     <>
-      <div className="grid grid-cols-[70%30%] gap-x-5">
+      <div className="grid grid-cols-[70%30%] gap-x-5 container mt-10">
         <div>
           <h1 className="text-xl font-semibold">Оферта:</h1>
           <h2 className="text-lg">Месечен абонамент за цялостна поддръжка на сайта</h2>
 
           <h1 className="text-xl font-semibold">Какво включва:</h1>
-          <ul className="list-disc ml-5">
-            <li>Създаване на 20 продукта на месец</li>
-            <li>Редактиране на 20 продукта на месец</li>
-            <li>10 функционалности на месец</li>
+          <ul className="ml-5 list-disc">
+            <li>Създаване на 10 продукта</li>
+            <li>Редактиране на 40 продукта</li>
+            <li>10 функционалности</li>
             <li>Корекции по функционалността на сайт</li>
             <li>Обновяване на остарели и уязвими модули</li>
           </ul>
+          <div className="max-w-2xl mt-4 text-sm"><span class="text-red-500">*</span> Всяка една услуга която включва бройки, всеки месец се добяват, а не започват наново
+          <br/>
+            
+            <span class="text-red-500">*</span> Примерно: Първият месец в абонамента сте имали &quot;Създаване на два продукта&quot;, обаче не е създанен нито един. Вторият месец ще имате четири продукта продукта
+            , демек се надчисляват, над тези които имате. 
+            <br/>
+            <span class="text-red-500">*</span> Същото се отнася и за функционалностите
+          </div>
+   
+
         </div>
-        <section className="p-6 bg-slate-200 rounded-xl">
+        <section className="self-start p-8 bg-white shadow-2xl rounded-xl">
           <section className="flex justify-between">
             <div>Цена</div>
-            <div>XXX лв.</div>
+            <div className="font-semibold">XXX лв.</div>
           </section>
           <section className="flex justify-between">
             <div>Плащане:</div>
-            <div>Месечно.</div>
+            <div className="font-semibold">Месечно.</div>
           </section>
           {/* Notes */}
-          <section className="mt-1 text-center">
+          <section className="my-4 text-sm text-center">
             * Абонамента започва при първо заплащане
           </section>
+          <Button text="Поръчай" className="w-full "/>
         </section>
       </div>
     </>
