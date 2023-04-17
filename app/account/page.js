b6b1d3f0995143db9a022bf2294aaddf48c6a723
StR0vErBG <xcreaw1@abv.bg>
2023-04-17 11:18:15 +0200

    adding no subscription

--- app/account/page.js
+++ app/account/page.js
@@ -5,9 +5,12 @@ export default function Page() {
   return (
     <>
       <div className="text-3xl font-semibold">Здравейте, Томи Костадинов! </div>
-      <div className="grid grid-cols-3 mt-10">
+      {/* <div className="grid grid-cols-3 mt-10">
         <Subscription />
-      </div>
+
+      </div> */}
+      <div className="mt-20 text-xl text-center">Нямате абонаменти</div>
+
     </>
   );
 }
