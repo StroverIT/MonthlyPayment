74eb38451526c286e9f1555386442f287896088c
StR0vErBG <xcreaw1@abv.bg>
2023-04-17 11:11:59 +0200

    adding shadow to the form

--- components/Forms/Create/Create.js
+++ components/Forms/Create/Create.js
@@ -6,10 +6,11 @@ import style from "./create.module.css";
 const Create = ({ className, onSubmit, children, title }) => {
   return (
     <div
-      className={`${style.form_wrapper} box-shadow  p-10 bg-white   rounded-[1.3rem] relative border-2 border-white `}
+      className={`${style.form_wrapper} shadow-xl  p-10 bg-white   rounded-[1.3rem] relative border-2 border-white `}
     >
       <h2 className="pb-3 text-xl font-semibold">{title}</h2>
       <div className={`relative z-10 ${className}`}>{children}</div>
+      
     </div>
   );
 };
