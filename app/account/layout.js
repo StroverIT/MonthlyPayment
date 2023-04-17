d6e9496e20a7cb3db6bafb894f796b2e635397d2
StR0vErBG <xcreaw1@abv.bg>
2023-04-17 21:31:16 +0200

    adding redirect

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,6 +1,14 @@
 import React from "react";
+import getSession from "../getSessionon";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }) => {
+const Layout = async ({ children }) => {
+  const session = await getSession(headers().get('cookie') ?? '');
+  if(!session){
+    redirect("/")
+  }
+
   return <main className="container">{children}</main>;
 };
 
