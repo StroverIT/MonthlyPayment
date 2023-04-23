aa41b77c7e092bd3098ec069677313f0d32231db
StR0vErBG <xcreaw1@abv.bg>
2023-04-23 10:30:15 +0200

    creating offer

--- db/models/Offer.js
+++ db/models/Offer.js
@@ -1,17 +1,34 @@
 import { Schema, model, models } from "mongoose";
 
-const tokenScheme = new Schema({
-  userId: {
+const OfferSchema = new Schema({
+  holderId: {
     type: Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-  token: {
+  title: {
     type: String,
     required: true,
   },
+  benefits: {
+    type: String,
+    
+  },
+  description:{
+    type: String,
+  },
+  pricing: {
+    type: Number,
+    required: true,
+  },
+  typeOfPayment: {
+    type: String,
+    enum: ["monthly", "weekly", "signle"],
+    required: true,
+  },
+
 });
 
-const Token = models.Token || model("Token", tokenScheme);
+const Offer = models.Offer || model("Offer", OfferSchema);
 
-export default Token;
+export default Offer;
