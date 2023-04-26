acfbf8b0932e2b3456fff0f9615117ca66ccdfa0
StR0vErBG <xcreaw1@abv.bg>
2023-04-26 12:33:54 +0200

    adding session user name

--- app/account/page.js
+++ app/account/page.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Subscription from "./subscription";
+import getSession from "../getSessionon";
+import { headers } from "next/headers";
 
-export default function Page() {
+export default async function Page() {
+  const session = await getSession(headers().get('cookie') ?? '');
   return (
     <>
-      <div className="text-3xl font-semibold">Здравейте, Томи Костадинов! </div>
+      <div className="text-3xl font-semibold">Здравейте, {session?.user?.name}! </div>
       {/* <div className="grid grid-cols-3 mt-10">
         <Subscription />
 
